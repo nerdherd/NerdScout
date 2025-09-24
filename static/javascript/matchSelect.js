d56d1425c4cd8f41338d90ebafa14c4a38e8a296
Magic Man <65839659+Magician357@users.noreply.github.com>
2025-09-24 10:35:07 -0700

    improve search for matches

--- static/javascript/matchSelect.js
+++ static/javascript/matchSelect.js
@@ -1,4 +1,7 @@
+const noneDiv = document.getElementById("none-div");
+
 const matchNumberInput = document.getElementById("matchNumberInput");
+const teamNumberInput = document.getElementById("teamNumberInput");
 
 const qmSelect = document.getElementById("qm-select");
 const efSelect = document.getElementById("ef-select");
@@ -8,8 +11,11 @@ const  fSelect = document.getElementById("f-select");
 
 function updateSearch(){
     let matchInput = matchNumberInput.value;
-    console.log(matchInput);
     let matchEmpty = (matchInput === "");
+
+    let teamInput = teamNumberInput.value;
+    let teamEmpty = (teamInput === "");
+
     let compLevels = [];
     if (qmSelect.checked){compLevels.push("qm");}
     if (efSelect.checked){compLevels.push("ef");}
@@ -17,16 +23,26 @@ function updateSearch(){
     if (sfSelect.checked){compLevels.push("sf");}
     if (fSelect.checked){compLevels.push("f");}
 
+    let noneFound = true;
+
     for (const matchDiv of document.querySelectorAll(".match-div")){
         matchDiv.classList.add("hide");
 
         let compLevel = matchDiv.dataset.complevel;
 
+        let teams = (matchDiv.dataset.redteams+","+matchDiv.dataset.blueteams).split(",");
 
+        let matchNum = (compLevel==="sf")?matchDiv.dataset.setnumber:matchDiv.dataset.matchnumber;
 
-        if ((matchEmpty || matchDiv.dataset.matchnumber.includes(matchInput)) && compLevels.includes(compLevel)){
+        if ((matchEmpty || matchNum.includes(matchInput)) && compLevels.includes(compLevel) && (teamEmpty || teams.includes(teamInput))){
             matchDiv.classList.remove("hide");
+            noneFound = false;
         }
+    }
+
+    noneDiv.classList.add("hide");
+    if (noneFound){
+        noneDiv.classList.remove("hide");
     }
 }
 
