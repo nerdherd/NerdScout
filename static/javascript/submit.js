e76214652f9fffeb02416a2daf270861ab173dc5
John-Cams <143854397+John-Cams@users.noreply.github.com>
2025-09-29 09:50:25 -0700

    Added missed scoring on scouting page

--- static/javascript/submit.js
+++ static/javascript/submit.js
@@ -8,23 +8,43 @@ const startPos3 = document.getElementById("SP-3")
 
 const leaveCheck = document.getElementById("leave")
 
-const autoLevels = [document.getElementById("aCL1"),
+const autoLevels = [
+    document.getElementById("aCL1"),
     document.getElementById("aCL2"),
     document.getElementById("aCL3"),
-    document.getElementById("aCL4")]
+    document.getElementById("aCL4")
+]
 
-const teleLevels = [document.getElementById("tCL1"),
+const teleLevels = [
+    document.getElementById("tCL1"),
     document.getElementById("tCL2"),
     document.getElementById("tCL3"),
-    document.getElementById("tCL4")]
+    document.getElementById("tCL4")
+]
 
 //autoProcessor teleProcessor autoNet teleNet
-const procNetLabels = [document.getElementById("aPr"),
+const procNetLabels = [
+    document.getElementById("aPr"),
     document.getElementById("tPr"),
     document.getElementById("aNe"),
-    document.getElementById("tNe")]
+    document.getElementById("tNe")
+]
+
+
+const missLabels = [
+    document.getElementById("aRM"),
+    document.getElementById("aPM"),
+    document.getElementById("aNM"),
+    document.getElementById("tRM"),
+    document.getElementById("tPM"),
+    document.getElementById("tNM"),
+]
+
+const missVals = [0,0,0,0,0,0]
 
 const endPosDDown = document.getElementById("endPos")
+
+const endPosWin = document.getElementById("attemptEP").value
 
 let matchNum = 0
 let compLevel = 0
@@ -36,20 +56,28 @@ let startpos = 0
 let autoLeave = false
 
 let autoReef = [0,0,0,0]
+let autoReefMiss = 0
 let teleReef = [0,0,0,0]
+let teleReefMiss = 0
 
 let autoProcessor = 0
+let autoProcessorMiss = 0
 let teleProcessor = 0
+let teleProcessorMiss = 0
 
 let autoNet = 0
+let autoNetMiss = 0
 let teleNet = 0
+let teleNetMiss = 0
 
 let endPos = 0
+let attemptedEndPos = 0
 
 let minorFouls = 0 
 let majorFouls = 0
 let comment = ""
 let scout = ""
+
 
 function showMatch() {
     autoTab.style.display = 'none';
@@ -119,6 +147,19 @@ function procNetSub(level){
     }
 }
 
+function missAdd(level){
+    missVals[level] += 1
+    missLabels[level].innerHTML = missVals[level] 
+}
+
+function missSub(){
+    if(miss[level] != 0){
+        missVals[level] += 1
+        missLabels[level].innerHTML = missVals[level] 
+    }
+}
+
+
 //-Minor +Minor -Major +Major
 function fouling(level){
     switch(level){
@@ -151,7 +192,12 @@ function submitData(tMatchNum, tCompLevel, tSetNum, tRobot){
     autoNet = procNetVars[2] 
     teleNet = procNetVars[3]
 
-    endPos = endPosDDown.value
+    if(endPosWin){
+        endPos = endPosDDown.value
+    } else {
+        attemptedEndPos = endPosDDown.value
+    }
+    
 
     comment = document.getElementById("Comments").value
 
@@ -166,12 +212,19 @@ function submitData(tMatchNum, tCompLevel, tSetNum, tRobot){
         "startPos": startpos,
         "autoLeave": autoLeave,
         "autoReef": autoReef,
+        "autoReefMiss": autoReefMiss,
         "teleReef": teleReef,
+        "teleReefMiss": teleReefMiss,
         "autoProcessor": autoProcessor,
+        "autoProcessorMiss": autoProcessorMiss,
         "teleProcessor": teleProcessor,
+        "teleProcessorMiss": teleProcessorMiss,
         "autoNet": autoNet,
+        "autoNet": autoNetMiss,
         "teleNet": teleNet,
+        "teleNetMiss": teleNetMiss,
         "endPos": endPos,
+        "attemptedEndPos": attemptedEndPos,
         "minorFouls": minorFouls,
         "majorFouls": majorFouls,
         "comment": comment,
