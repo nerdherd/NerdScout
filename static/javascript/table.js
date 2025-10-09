b8f2db08905fe74e95d94a101bfd8bb832821944
Magic Man <65839659+Magician357@users.noreply.github.com>
2025-10-09 11:27:13 -0700

    download table as csv

--- static/javascript/table.js
+++ static/javascript/table.js
@@ -27,3 +27,37 @@ function updateSort(){
         mainTable.appendChild(element);
     });;
 }
+
+function download_table_as_csv(separator = ',') {
+    var rows = document.querySelectorAll('table#main-table tr');
+    var csv = [];
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll('td, th');
+        for (var j = 0; j < cols.length; j++) {
+            console.log(cols[j].nodeName);
+            if (cols[j].nodeName==="TH"){
+                // Clean innertext to remove multiple spaces and jumpline (break csv)
+                var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+                // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+                data = data.replace(/"/g, '""');
+                // Push escaped string
+                row.push('"' + data + '"');
+            } else {
+                var data = cols[j].innerText;
+                row.push(data);
+            }
+        }
+        csv.push(row.join(separator));
+    }
+    var csv_string = csv.join('\n');
+    // Download it
+    var filename = 'export_teams_' + new Date().toLocaleDateString() + '.csv';
+    var link = document.createElement('a');
+    link.style.display = 'none';
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
