ffa7f86625a0c2b4ae59d7d96871ea7c9a7341d4
Magic Man <65839659+Magician357@users.noreply.github.com>
2026-01-11 12:32:12 -0800

    create functionality for submit

--- static/javascript/submit.js
+++ static/javascript/submit.js
@@ -1,64 +1,75 @@
+function toggleActive(id){
+    document.getElementById(id).classList.toggle("active");
+}
 
-// function submitData(tMatchNum, tCompLevel, tSetNum, tRobot){
+function incrementCounter(id,isPositive){
+    const curElement = document.getElementById(id);
+    if (isPositive) curElement.value++;
+    else if (curElement.value > 0) curElement.value--;
+}
 
-//     matchNum = tMatchNum
-//     compLevel = tCompLevel
-//     setNum = tSetNum
-//     robot = tRobot
+function getId(id){return document.getElementById(id).value;}
 
+function submitData(matchNum, compLevel, setNum, robot){
 
-//     let startPos = 4-startPosSlider.value;
-//     autoLeave = leaveCheck.checked;
+    // matchNum = tMatchNum
+    // compLevel = tCompLevel
+    // setNum = tSetNum
+    // robot = tRobot
 
-//     autoProcessor = procNetVars[0] 
-//     teleProcessor = procNetVars[1]
-//     autoNet = procNetVars[2] 
-//     teleNet = procNetVars[3]
+    cannedComments = []
 
-//     const endPosWin = !(document.getElementById("attemptEP").checked);
+    for (const button of document.querySelectorAll(".canned-button.active")){
+        cannedComments.push(button.dataset.text);
+    }
 
-//     attemptedEndPos = endPosDDown.value-1;
-
-//     // if(endPosWin){
-//     //     endPos = endPosDDown.value-1
-//     // } else {
-//     //     endPos = 1; // assume they parked
-//     // }
-
-//     cannedComments = []
-
-//     for (const button of document.querySelectorAll(".canned-button.active")){
-//         cannedComments.push(button.dataset.text);
-//     }
-
-//     comment = document.getElementById("comments").value
-
-//     autoReefMiss = missVals[0];
-//     autoProcessorMiss = missVals[1];
-//     autoNetMiss = missVals[2];
-
-//     teleReefMiss = missVals[3];
-//     teleProcessorMiss = missVals[4];
-//     teleNetMiss = missVals[5];
+    // comment = document.getElementById("comments").value
 
 
-//     rawData = {
-//         "preloadFuel":preloadFuel,
-//         "auto
-//     };
-//     data = JSON.stringify(rawData)
-//     fetch(window.location.href, {
-//     method: "POST",
-//     body: data, 
-//     headers: {
-//         "Content-type": "application/json; charset=UTF-8"
-//     }
-//     }).then(response =>{
-//         if (response.ok){
-//             alert("Succesfully submitted")
-//             redirect_to_match()
-//         } else{
-//             alert("There was an error submitting.");
-//         }
-//     });;
-// }
+    rawData = {
+        "matchNum": matchNum,
+        "compLevel": compLevel,
+        "setNum": setNum, 
+        "robot": robot,
+        "startPosX": 0.0,
+        "startPosY": 0.0,
+        "preloadFuel": getId("preloadFuel"),
+        "autoFuel": getId("autoFuel"),
+        "autoFuelMiss": getId("autoFuelMiss"),
+        "autoClimb": document.getElementById("autoClimb").checked,
+        "firstShift": document.getElementById("firstShift").checked,
+        "transitionFuel": getId("transitionFuel"),
+        "transitionFuelMiss": getId("transitionFuelMiss"),
+        "shift1Fuel": getId("shift1Fuel"),
+        "shift1FuelMiss": getId("shift1FuelMiss"),
+        "shift2Fuel": getId("shift2Fuel"),
+        "shift2FuelMiss": getId("shift2FuelMiss"),
+        "shift3Fuel": getId("shift3Fuel"),
+        "shift3FuelMiss": getId("shift3FuelMiss"),
+        "shift4Fuel": getId("shift4Fuel"),
+        "shift4FuelMiss": getId("shift4FuelMiss"),
+        "endgameFuel": getId("endgameFuel"),
+        "endgameFuelMiss": getId("endgameFuelMiss"),
+        "attemptedEndPos": getId("attemptedEndPos"),
+        "minorFouls": 1,
+        "majorFouls": 1,  
+        "comment": getId("comments"),  
+        "cannedComments": cannedComments,  
+    };
+    data = JSON.stringify(rawData);
+    console.log(data);
+    fetch(window.location.href, {
+        method: "POST",
+        body: data, 
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then(response =>{
+        if (response.ok){
+            alert("Succesfully submitted")
+            redirect_to_match()
+        } else{
+            alert("There was an error submitting.");
+        }
+    });;
+}
